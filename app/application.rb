a953d9546920120750b45748b6b64e68d3700a02
Huy Do <huyddo40@gmail.com>
2019-08-30 17:34:21 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    current_time = time
+    current_time = Time.now
     binding.pry
     if current_time < 12
       resp.write "Good Morning!"
